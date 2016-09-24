e50f087b0a570f784af156134d13b36835d610b2
Kelvin Lau <kelvin.kl.lau@gmail.com>
2016-09-23 19:20:24 -0700

    Initial Commit.

---
+++ Intentify/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Intentify
+//
+//  Created by Kelvin Lau on 2016-09-23.
+//  Copyright © 2016 Kelvin. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder {
+  var window: UIWindow?
+}
+
+// MARK: - UIApplicationDelegate
+extension AppDelegate: UIApplicationDelegate {
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+    return true
+  }
+}
---
+++ Intentify/LocationManager.swift
@@ -0,0 +1,9 @@
+//
+//  LocationManager.swift
+//  Intentify
+//
+//  Created by Kelvin Lau on 2016-09-23.
+//  Copyright © 2016 Kelvin. All rights reserved.
+//
+
+import Foundation
